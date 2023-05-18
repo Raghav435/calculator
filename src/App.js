060445cdbab4a15e1112b6589a1cf5e25a565441
Raghav Shukla <shuklaraghav435@gmail.com>
2023-05-18 13:49:18 +0530

    some modification in button and screen file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
+import CalcProvider from "./context/CalcContext";
 
 const btnValues = [
   ["C", "+-", "%", "/"],
@@ -11,10 +12,9 @@ const btnValues = [
   [1, 2, 3, "+"],
   [0, ".", "="],
 ];
-
 function App() {
   return (
-    <div className="App">
+    <CalcProvider>
       <Wrapper>
         <Screen />
         <ButtonBox>
@@ -22,9 +22,8 @@ function App() {
             <Button value={btn} key={i} />
           ))}
         </ButtonBox>
-        <h1></h1>
       </Wrapper>
-    </div>
+    </CalcProvider>
   );
 }
 
